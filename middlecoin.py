7c7210dbb3d2f1a8845e63c24cf42c0a95e42711
hypnol <jonjez@gmail.com>
2014-01-29 23:10:23 +0100

    Update middlecoin.py

--- middlecoin.py
+++ middlecoin.py
@@ -14,11 +14,12 @@ BTC: 1EN27w1mMHp6mTaHk5RNoSVo7ZGwbMeH45
 from bs4 import BeautifulSoup
 from urllib2 import urlopen
 import json
+import time
 
 BASE_URL = "http://www.middlecoin.com/allusers.html"
-HISTORY_URL = "http://www.middlecoin.com/reports/{your btc wallet address}.html"
-WALLET = "{your btc wallet address}"
-MTGOX = "http://data.mtgox.com/api/1/BTC{your currency(example BTCSEK)}/ticker"
+HISTORY_JSON = "http://www.middlecoin.com/reports/{your btc wallet address}.json"
+WALLET = "{Your wallet address}"
+MTGOX = "http://data.mtgox.com/api/1/{your currency(example BTCSEK)}/ticker"
 
 def get_mywallet(url):
     GLOBAL = WALLET
@@ -32,33 +33,55 @@ def get_mywallet(url):
         print i,a.get_text()
     print "\n"
 
-def get_history(url):
+def get_test_hist(url):
     html = urlopen(url).read()
-    soup = BeautifulSoup(html, "lxml")
-    tr = [a.get_text() for a in soup.find_all('tr')]
-    for i in tr:
-        i.replace(" ", '', 1)
-        print i.replace("\n"," ")
+    test = json.loads(html)
+    total = test['report']
+    for x in total:
+        transid = x['txid']
+        unixtime = time.ctime(x['time'])
+        amount = x['amount']
+        print transid,"/", unixtime,"/", amount, "BTC"
 
-def get_total_in_sek(url):
-    data = {}
-    html = urlopen(url).read()
-    soup = BeautifulSoup(html, "lxml")
-    tr = [a.get_text() for a in soup.find_all('tr')]
-    for i in tr:
-        data.setdefault(i.replace("\n",""))
-    for i in data:
-        if i.startswith('Total'):
-            return float(i.strip('Total'))
 
-def get_mtgox(url):
+
+def get_mtgox_sek_high(url):
     html = urlopen(url).read()
     test = json.loads(html)
     return float(test['return']['high']['value'])
 
+def get_mtgox_sek_low(url):
+    html = urlopen(url).read()
+    test = json.loads(html)
+    return float(test['return']['low']['value'])
+
+def get_mtgox_sek_avg(url):
+    html = urlopen(url).read()
+    test = json.loads(html)
+    return float(test['return']['avg']['value'])
+
+def get_total_btc(url):
+    html = urlopen(url).read()
+    test = json.loads(html)
+    total = test['total']
+    return float(total)
 
 
 get_mywallet(BASE_URL)
-get_history(HISTORY_URL)
-get_total_in_sek(HISTORY_URL)
-print " Total",get_mtgox(MTGOX) * get_total_in_sek(HISTORY_URL), "SEK (MTGOX)"
+print "Transaction ID: \t\t\t\t\t\t\t\t\t\t\t\t   Time:   \t\t\t\t\t  Amount:"
+get_test_hist(HISTORY_JSON)
+print "Total BTC:",get_total_btc(HISTORY_JSON)
+print "\n"
+
+print "BTC Stats:"
+print "************************************************************************************************************"
+print "Mtgox High:",get_mtgox_sek_high(MTGOX), "SEK"
+print "Mtgox Low:",get_mtgox_sek_low(MTGOX), "SEK"
+print "Mtgox Avg:",get_mtgox_sek_avg(MTGOX), "SEK"
+print "\n"
+
+print "Total:"
+print "************************************************************************************************************"
+print "High:",get_mtgox_sek_high(MTGOX) * get_total_btc(HISTORY_JSON), "SEK (MTGOX)"
+print "Low:",get_mtgox_sek_low(MTGOX) * get_total_btc(HISTORY_JSON), "SEK (MTGOX)"
+print "Avg:",get_mtgox_sek_avg(MTGOX) * get_total_btc(HISTORY_JSON), "SEK (MTGOX)"
